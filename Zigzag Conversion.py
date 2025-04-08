d4dd70f65ca92d3ebc20a186b34fb7ad9020ba1c
Logesh08 <logeshkrishna08@gmail.com>
2025-04-08 23:13:27 +0530

    08-Apr-2025

---
+++ Zigzag Conversion.py
@@ -0,0 +1,60 @@
+# LeetCode Problem 6: Zigzag Conversion
+# https://leetcode.com/problems/zigzag-conversion/
+
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+# Write the code that will take a string and make this conversion given a number of rows:
+
+# string convert(string s, int numRows);
+ 
+
+# Example 1:
+
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# Example 2:
+
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# Example 3:
+
+# Input: s = "A", numRows = 1
+# Output: "A"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+
+
+
+# This is my first solution, I tired hard to solve in O(n)
+# Idk if its a good way or not...
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        n=len(s)
+        if numRows==1: return s
+        ans=''
+        base = (numRows-1) * 2
+        for i in range(numRows):
+            for cur in range(i,n,base):
+                ans += s[cur]
+                right = cur + base - i*2
+                if i>0 and i<numRows-1 and right < n:
+                    ans += s[right]
+
+        return ans
+
