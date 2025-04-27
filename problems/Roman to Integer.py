92247ba64248086ea2f1f2508b8ba85624805b72
Logesh08 <logeshkrishna08@gmail.com>
2025-04-27 16:05:16 +0530

    27-Apr-2025
    closes #22

---
+++ problems/Roman to Integer.py
@@ -0,0 +1,101 @@
+# LeetCode Problem 13: Roman to Integer
+# https://leetcode.com/problems/roman-to-integer/
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9. 
+# X can be placed before L (50) and C (100) to make 40 and 90. 
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+ 
+
+# Example 1:
+
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+# Example 2:
+
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+# Example 3:
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+
+
+
+
+# I was not able to imagine of solving this problem because 
+# I thought what to do in case of IX(9), i mean just track or something
+# But later realised we can subtract I(1) from X(10) and which is 9
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romans = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        ans = 0
+        for i in range(len(s)-1):
+            ch = s[i]
+            if romans[ch] < romans[s[i+1]]:
+                ans += romans[ch]
+            else:
+                ans -= romans[ch]
+        
+        return ans + romans[s[-1]]
+        
+
+
+# Can do the same solution optimised by using zip
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romans = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        ans = 0
+        for ch, next_ch in zip(s, s[1:]):
+            if romans[ch] < romans[next_ch]:
+                ans -= romans[ch]
+            else:
+                ans += romans[ch]
+        
+        return ans + romans[s[-1]]
