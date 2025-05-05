b0e3493aa9f58a726457c906b2091b53a6407361
Logesh08 <logeshkrishna08@gmail.com>
2025-05-05 17:14:57 +0530

    05-May-2025
    closes #88

--- problems/Push Dominoes.py
+++ problems/Push Dominoes.py
@@ -197,3 +197,28 @@ class Solution:
     s = re.sub(r'(?<=R)(\.+)(?=L)', lambda m:(q:=len(m[1]))//2*'R'+q%2*'.'+q//2*'L', s)
     
     return s[1:-1]
+  
+
+
+# It can also be solved this way
+# But mine is more efficient, because of the two pointers supremacy
+
+class Solution(object):
+    def pushDominoes(self, s):
+        s = 'L' + s + 'R'
+        res = ''
+        prev = 0
+        for curr in range(1, len(s)):
+            if s[curr] == '.':
+                continue
+            span = curr - prev - 1
+            if prev > 0:
+                res += s[prev]
+            if s[prev] == s[curr]:
+                res += s[prev] * span
+            elif s[prev] == 'L' and s[curr] == 'R':
+                res += '.' * span
+            else:
+                res += 'R' * (span // 2) + '.' * (span % 2) + 'L' * (span // 2)
+            prev = curr
+        return res
