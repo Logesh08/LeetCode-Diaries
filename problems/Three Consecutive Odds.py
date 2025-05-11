bee24b6856009f3d9127943a1ee15f18857826e0
Logesh08 <logeshkrishna08@gmail.com>
2025-05-11 17:59:59 +0530

    11-May-2025
    closes #114

---
+++ problems/Three Consecutive Odds.py
@@ -0,0 +1,36 @@
+# LeetCode Problem 1550: Three Consecutive Odds
+# https://leetcode.com/problems/three-consecutive-odds/
+
+
+# Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+
+# Example 1:
+
+# Input: arr = [2,6,4,1]
+# Output: false
+# Explanation: There are no three consecutive odds.
+# Example 2:
+
+# Input: arr = [1,2,34,3,4,5,7,23,12]
+# Output: true
+# Explanation: [5,7,23] are three consecutive odds.
+ 
+
+# Constraints:
+
+# 1 <= arr.length <= 1000
+# 1 <= arr[i] <= 1000
+
+
+
+
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        if len(arr) < 3: return False
+        for i in range(0,len(arr)-2):
+            if arr[i]%2 and arr[i+1]%2 and arr[i+2]%2:
+                return True
+        return False
