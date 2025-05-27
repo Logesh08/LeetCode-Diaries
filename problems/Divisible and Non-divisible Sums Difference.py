fd4ad0da211fd87753a6faa826fcb1909b9f0235
Logesh08 <logeshkrishna08@gmail.com>
2025-05-27 13:27:41 +0530

    27-May-2025
    closes #146

--- problems/Divisible and Non-divisible Sums Difference.py
+++ problems/Divisible and Non-divisible Sums Difference.py
@@ -42,3 +42,18 @@
 # Constraints:
 
 # 1 <= n, m <= 1000
+
+
+
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = num2 = 0
+
+        for i in range(1,n+1):
+            if i%m == 0:
+                num2 += i
+            else:
+                num1 += i
+
+        return num1 - num2
