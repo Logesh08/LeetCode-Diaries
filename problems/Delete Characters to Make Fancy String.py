8730c49401aeb7fcd132b5c29592b5e1236ce6ba
Logesh08 <logeshkrishna08@gmail.com>
2025-07-21 12:13:41 +0530

    21-Jul-2025
    closes #166

---
+++ problems/Delete Characters to Make Fancy String.py
@@ -0,0 +1,54 @@
+# LeetCode Problem 1957: Delete Characters to Make Fancy String
+# Problem Link: https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+
+
+# A fancy string is a string where no three consecutive characters are equal.
+
+# Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+# Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+ 
+
+# Example 1:
+
+# Input: s = "leeetcode"
+# Output: "leetcode"
+# Explanation:
+# Remove an 'e' from the first group of 'e's to create "leetcode".
+# No three consecutive characters are equal, so return "leetcode".
+# Example 2:
+
+# Input: s = "aaabaaaa"
+# Output: "aabaa"
+# Explanation:
+# Remove an 'a' from the first group of 'a's to create "aabaaaa".
+# Remove two 'a's from the second group of 'a's to create "aabaa".
+# No three consecutive characters are equal, so return "aabaa".
+# Example 3:
+
+# Input: s = "aab"
+# Output: "aab"
+# Explanation: No three consecutive characters are equal, so return "aab".
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists only of lowercase English letters.
+
+
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        ans = [s[0]]
+        freqPrev = 1
+        for i in range(1,len(s)):
+            if s[i] == s[i-1]:
+                freqPrev += 1
+            else:
+                freqPrev = 1
+            if freqPrev < 3:
+                ans.append(s[i])
+
+        return ''.join(ans)
