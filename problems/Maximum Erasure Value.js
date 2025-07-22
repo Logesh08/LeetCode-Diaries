a50e5c2a5b11eff38e5fc641bc7b2b4b971752ad
Logesh08 <logeshkrishna08@gmail.com>
2025-07-22 15:48:56 +0530

    22-Jul-2025
    closes #170

---
+++ problems/Maximum Erasure Value.js
@@ -0,0 +1,72 @@
+// LeetCode Problem 1695: Maximum Erasure Value
+// https://leetcode.com/problems/maximum-erasure-value/
+
+
+// You are given an array of positive integers nums and want to erase a subarray containing unique elements.
+// The score you get by erasing the subarray is equal to the sum of its elements.
+
+// Return the maximum score you can get by erasing exactly one subarray.
+
+// An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is,
+// if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+
+// Example 1:
+// Input: nums = [4,2,4,5,6]
+// Output: 17
+// Explanation: The optimal subarray here is [2,4,5,6].
+
+// Example 2:
+// Input: nums = [5,2,1,2,5,2,1,2,5]
+// Output: 8
+// Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+
+// Constraints:
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+
+
+
+// class Solution:
+//     def maximumUniqueSubarray(self, nums: List[int]) -> int:
+//         left = 0
+//         seen = set()
+//         curSum = 0
+//         maxSum = 0
+
+//         for val in nums:
+//             while val in seen:
+//                 seen.remove(nums[left])
+//                 curSum -= nums[left]
+//                 left += 1
+
+//             seen.add(val)
+//             curSum += val
+//             maxSum = max(maxSum, curSum)
+
+//         return maxSum
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumUniqueSubarray = function(nums) {
+    let left = 0, curSum = 0, maxSum = 0;
+    let seen = new Set();
+    for (let val of nums) {
+        while (seen.has(val)) {
+            seen.delete(nums[left]);
+            curSum -= nums[left];
+            left++;
+        }
+        seen.add(val);
+        curSum += val;
+        maxSum = Math.max(maxSum, curSum);
+    }
+    return maxSum;
+};
