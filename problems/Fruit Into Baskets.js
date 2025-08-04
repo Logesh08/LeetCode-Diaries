d900f24ce2446f648fd498eb52303e680093c934
Logesh08 <logeshkrishna08@gmail.com>
2025-08-04 14:37:28 +0530

    04-Aug-2025
    closes #178

---
+++ problems/Fruit Into Baskets.js
@@ -0,0 +1,65 @@
+// LeetCode Problem 904: Fruit Into Baskets
+// https://leetcode.com/problems/fruit-into-baskets/
+
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+ 
+
+// Example 1:
+
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+// Example 2:
+
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+// Example 3:
+
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+ 
+
+// Constraints:
+
+// 1 <= fruits.length <= 105
+// 0 <= fruits[i] < fruits.length
+
+
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+    let left = 0, maxLen = 0;
+    let window = new Map();
+    for (let right = 0; right < fruits.length; right++) {
+        
+        window.set( fruits[right], 
+            (window.get(fruits[right])||0) + 1);
+
+        while(window.size > 2){
+            window.set(fruits[left], 
+                window.get(fruits[left]) - 1);
+            if(window.get(fruits[left]) === 0){
+                window.delete(fruits[left]);
+            }
+            left ++;
+        }
+
+        maxLen = Math.max(right - left + 1, maxLen);
+    }
+    return maxLen;
+};
