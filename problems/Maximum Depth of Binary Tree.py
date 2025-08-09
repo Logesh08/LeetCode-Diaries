ba09b758f7d341bb749ed8269ef77dcfa68dcff6
Logesh08 <logeshkrishna08@gmail.com>
2025-08-09 16:29:06 +0530

    09-Aug-2025
    closes #235

--- problems/Maximum Depth of Binary Tree.py
+++ problems/Maximum Depth of Binary Tree.py
@@ -32,3 +32,39 @@ class Solution:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+
+# No iteration with deque
+
+from collections import deque
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = deque([root])
+        depth = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:  q.append(node.left)
+                if node.right: q.append(node.right)
+            depth += 1
+        return depth
+
+
+# No iteration with stack
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        ans = 0
+        while stack:
+            node, d = stack.pop()
+            ans = max(ans, d)
+            if node.left:  stack.append((node.left,  d + 1))
+            if node.right: stack.append((node.right, d + 1))
+        return ans
