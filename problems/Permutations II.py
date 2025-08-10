ec0ee72914bbcc6e5be38a9069a4853db56ab42b
Logesh08 <logeshkrishna08@gmail.com>
2025-08-10 16:42:54 +0530

    10-Aug-2025
    closes #249

---
+++ problems/Permutations II.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                # When we get our desired length, we can just return
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                # Skip using the used ones
+                if used[i]:
+                    continue
+                # Skip duplicate recursion at depth
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]: 
+                    continue
+
+                used[i] = True
+                path.append([i])
+
+                backtrack(path)
+                # Here we will first fill all the elments like
+                # [1,2,3,x] and start replacing from last like
+                # [1,2,3,y], [1,2,3,z] and then go a step back
+                # and do the same while there are unused
+
+                path.pop()
+                used[i] = False
+
+        # Start with an empty [] which we will build each ans
+        backtrack([])
+        return res
