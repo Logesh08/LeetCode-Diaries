1a1c8718cc7b89fa69e5d82d7c82cd7cc2971584
Logesh08 <logeshkrishna08@gmail.com>
2025-08-29 16:35:47 +0530

    29-Aug-2025
    closes #341

--- problems/Min Stack.py
+++ problems/Min Stack.py
@@ -41,3 +41,27 @@ class MinStack:
 
 
 # Uinsg the diff stack method
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = float("inf")
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append(0)
+            self.min = val
+        else:
+            self.stack.append(val - self.min)
+            self.min = min(self.min, val)
+
+    def pop(self) -> None:
+        pop = self.stack.pop()
+        if pop < 0:
+            self.min = self.min - pop
+
+    def top(self) -> int:
+        return self.stack[-1] + self.min if self.stack[-1] > 0 else self.min
+
+    def getMin(self) -> int:
+        return self.min
