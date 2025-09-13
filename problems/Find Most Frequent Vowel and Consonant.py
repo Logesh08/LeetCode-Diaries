514468e6afa3e9d34a602e950404bcd89b91dde0
Logesh08 <logeshkrishna08@gmail.com>
2025-09-13 23:06:03 +0530

    13-Sep-2025
    closes #400

--- problems/Find Most Frequent Vowel and Consonant.py
+++ problems/Find Most Frequent Vowel and Consonant.py
@@ -13,3 +13,25 @@ class Solution:
                 consFreq[c] += 1
                 consFreqMax = max(consFreqMax, consFreq[c])
         return vowFreqMax + consFreqMax
+    
+
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        
+        freq_map = defaultdict(int)
+        for char in s:
+            freq_map[char] += 1
+        
+        vowel_max = 0
+        consonant_max = 0
+        vowels = "aeiou"
+        for key, value in freq_map.items():
+            if key in vowels:
+                if value > vowel_max:
+                    vowel_max = value
+            else:
+                if value > consonant_max:
+                    consonant_max = value
+        
+        return vowel_max + consonant_max
