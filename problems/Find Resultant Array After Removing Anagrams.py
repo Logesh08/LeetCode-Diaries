1a0a1cb71ab3120916c792da1e80ba16fbb0a6af
Logesh08 <logeshkrishna08@gmail.com>
2025-10-14 08:15:50 +0530

    14-Oct-2025
    closes #470

--- problems/Find Resultant Array After Removing Anagrams.py
+++ problems/Find Resultant Array After Removing Anagrams.py
@@ -21,3 +21,19 @@ class Solution(object):
             if a != b:
                 ans.append(words[i])
         return ans
+    
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        res = []
+        stack = []
+
+        stack.append(''.join(sorted(words[0])))
+        res.append(words[0])
+
+        for i in range(1, len(words)):
+            curr = ''.join(sorted(words[i]))
+            if stack[-1] != curr:
+                stack.append(curr)
+                res.append(words[i])
+        return res
