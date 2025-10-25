033d348985e222dbaa6cb3b41698cd57f3ea44b5
Logesh08 <logeshkrishna08@gmail.com>
2025-10-25 13:51:02 +0530

    25-Oct-2025
    closes #508

--- problems/Calculate Money in Leetcode Bank.py
+++ problems/Calculate Money in Leetcode Bank.py
@@ -13,6 +13,17 @@ class Solution:
             else:
                 ctr += 1
         return ans
+    
+
+
+class Solution:
+    def triSum(self, n: int) -> int:
+        return (n * (n + 1)) >> 1
+
+    def totalMoney(self, days: int) -> int:
+        nWeeks, rDays = divmod(days, 7)        
+        return self.triSum(days) - 42 * self.triSum(nWeeks - 1) - 6 * nWeeks * rDays
+
 
     
 print(Solution().totalMoney(7))
