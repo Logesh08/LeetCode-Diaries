de569063ef0fa81ae0c1d8a895470608e8e1c0eb
Logesh08 <logeshkrishna08@gmail.com>
2025-10-29 23:59:04 +0530

    29-Oct-2025
    closes #537

--- problems/Koko Eating Bananas.py
+++ problems/Koko Eating Bananas.py
@@ -1,7 +1,24 @@
 from typing import List
 from math import ceil
 
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
 
+        while left < right:
+            mid = left + (right - left) // 2
+            k = mid
+            hour = 0
+            for pile in piles:
+                hour += ceil(pile / k)
+            if hour > h:
+                left = mid + 1
+            else:
+                right = mid
+                # notice that its mid and not mid - 1, because inclusive.
+
+        return left
 
 
 
