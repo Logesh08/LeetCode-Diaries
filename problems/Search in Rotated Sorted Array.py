5198558c7b5b7e11630f69058404f62adb081f6e
Logesh08 <logeshkrishna08@gmail.com>
2025-10-31 15:52:28 +0530

    31-Oct-2025
    closes #547

--- problems/Search in Rotated Sorted Array.py
+++ problems/Search in Rotated Sorted Array.py
@@ -6,13 +6,20 @@ class Solution:
         left = 0
         right = len(nums) - 1
 
-        while left < right:
+        while left <= right:
             mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                right = mid
+            # First case, where we are searching in sorted array
+            if nums[mid] >= nums[left]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
             else:
-                left = mid + 1
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
 
         return -1
