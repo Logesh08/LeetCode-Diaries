3c2d975a902bd2884b9de8702dce38fae534dd74
Logesh08 <logeshkrishna08@gmail.com>
2025-11-02 12:16:24 +0530

    02-Nov-2025
    closes #579

--- problems/Permutation in String.py
+++ problems/Permutation in String.py
@@ -1,10 +1,35 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        targetMap = {}
-        windowMap = {}
-        for c in s1:
-            targetMap[c] = targetMap.get(c, 0) + 1
+        if len(s1) > len(s2):
+            return False
         
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1Count[i] == s2Count[i] else 0
+
         left = 0
-        for right in range(len(s1)-1, len(s2)):
+        for right in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
             
+            index = ord(s2[right]) - ord('a')
+            s2Count[index] += 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+            elif s2Count[index] == s1Count[index] + 1:
+                matches -= 1
+
+            index = ord(s2[left]) - ord('a')
+            s2Count[index] -= 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+            elif s2Count[index] == s1Count[index] - 1:
+                matches -= 1
+
+            left += 1
+        return matches == 26
