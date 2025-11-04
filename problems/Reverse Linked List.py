2b589cdadb494dd2e44d3a78c668bc9d8fa65e7a
Logesh08 <logeshkrishna08@gmail.com>
2025-11-04 20:52:49 +0530

    04-Nov-2025
    closes #601

--- problems/Reverse Linked List.py
+++ problems/Reverse Linked List.py
@@ -6,6 +6,7 @@ class ListNode:
 from typing import Optional
 
 
+## My Own hard worked, brain squeezed solution ahhhhHhhhhhh!!!!
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
@@ -17,3 +18,19 @@ class Solution:
                 break
             head = next
         return head
+    
+
+# Ahh, seems like i can return the prev itself!
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
+    
+
+
+# 
