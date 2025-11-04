9ecbaa20e17ff76141d988b84246173b46b9f98c
Logesh08 <logeshkrishna08@gmail.com>
2025-11-04 13:48:49 +0530

    04-Nov-2025
    closes #595

--- problems/Sliding Window Maximum.py
+++ problems/Sliding Window Maximum.py
@@ -1,43 +1,25 @@
 from typing import List
+from collections import deque
 
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res = [nums[0]]  # Initialising first element of list as max for first window
-        prevMaxInd = 0
-        nextMax = float("-inf")
-        nextMaxInd = -1
+        window = deque()
+        res = []
 
-        for i in range(1, k):
-            if nums[i] >= res[-1]:
-                prevMaxInd = i
-                res[-1] = nums[i]
+        left = 0
+        for right in range(len(nums)):
+            while window and nums[window[-1]] <= nums[right]:
+                window.pop()
+            window.append(right)
 
-        left = 1
-        for i in range(k, len(nums)):
-            if prevMaxInd >= left and nums[i] >= res[-1]:
-                res.append(nums[i])
-                prevMaxInd = i
-            else:
-                # Case where the prevMax moves out of the window
-                if nums[i] >= nextMax:
-                    res.append(nums[i])
-                    prevMaxInd = i
-                else:
-                    res.append(nextMax)
-                    prevMaxInd = nextMaxInd
+            if left > window[0]:
+                window.popleft()
 
-            if i > prevMaxInd and nums[i] >= nextMax:
-                nextMax = nums[i]
-                nextMaxInd = i
-            else:
-                nextMax = float("-inf")
-
-            left += 1
-
+            if (right + 1) >= k:
+                res.append(nums[window[0]])
+                left += 1
 
         return res
-
-
 
 print(Solution().maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
