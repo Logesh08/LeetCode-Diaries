ab51a0c0864df5ba0fdd66bfedecd07d1ea9cff0
Logesh08 <logeshkrishna08@gmail.com>
2025-11-05 09:36:43 +0530

    05-Nov-2025
    closes #617

--- problems/Reorder List.py
+++ problems/Reorder List.py
@@ -6,11 +6,42 @@ from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
-        self.next = next
+        self.next: ListNode = next
 
 
 
+# O(1) memory solution
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow = head
+        fast = head.next
 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+
+        second = slow.next # This is right half
+        prev = slow.next = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
+
+# Not recommended Deque solution, cuz it is O(n) memory
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
