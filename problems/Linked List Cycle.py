063fce64e7f3df899102f0c67f32f1829ded99ad
Logesh08 <logeshkrishna08@gmail.com>
2025-11-05 07:54:08 +0530

    05-Nov-2025
    closes #609

---
+++ problems/Linked List Cycle.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
+    
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
