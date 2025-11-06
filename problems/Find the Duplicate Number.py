ae5701638f3da59214d40b25850dcc89bbc0bc18
Logesh08 <logeshkrishna08@gmail.com>
2025-11-06 11:07:23 +0530

    06-Nov-2025
    closes #635

--- problems/Find the Duplicate Number.py
+++ problems/Find the Duplicate Number.py
@@ -1,6 +1,32 @@
 from typing import List
 
 
+# Yay I learnt floyd's Algorithm today
+
+# This is not standard case but this problem can be solved by Floyd's Algorithm
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast = slow = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+                # Fast and slow intersection point
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                break
+                # Old and new slow intersection point
+                # This is the place where the cycle begins
+                # Cyble begins means, 2 or more nodes point to a same node
+                # In this case for ex nums[i] = 2, nums[j] = 2, since both point here
+                # This is where the cycle begins and the answer is also the same!
+
+        return nums[slow]
 
 
 
