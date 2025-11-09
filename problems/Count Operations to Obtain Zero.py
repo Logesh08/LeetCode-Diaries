a75cc203b0efbb0f9ad9bd28734f9bc2325937ec
Logesh08 <logeshkrishna08@gmail.com>
2025-11-09 10:59:52 +0530

    09-Nov-2025
    closes #659

---
+++ problems/Count Operations to Obtain Zero.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        operations = 0
+
+        while num1 and num2:
+            if num1 > num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            operations += 1
+
+        return operations
+    
+
+# Best solution
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        operations = 0
+
+        while num1 and num2:
+            if num1 > num2:
+                operations += num1 // num2
+                num1 %= num2
+            else:
+                operations += num2 // num1
+                num2 %= num1
+
+        return operations
