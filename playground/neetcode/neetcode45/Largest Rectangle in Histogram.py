5eab8df3c11828b9d61931ace43d281e239b053a
Logesh08 <logeshkrishna08@gmail.com>
2025-11-14 23:32:49 +0530

    14-Nov-2025
    closes #707

--- playground/neetcode/neetcode45/Largest Rectangle in Histogram.py
+++ playground/neetcode/neetcode45/Largest Rectangle in Histogram.py
@@ -1,8 +1,20 @@
 from typing import List
 
-
+# 3min 21 secs
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
-        largestRectange = 0
+        n = len(heights)
+        maxArea = 0
 
+        for i in range(n + 1):
+            while stack and (i == n or heights[stack[-1]] >= heights[i]):
+                height = heights[stack.pop()]
+                if not stack:
+                    width = i
+                else:
+                    width = i - stack[-1] - 1
+                maxArea = max(maxArea, height * width)
+            stack.append(i)
+
+        return maxArea
