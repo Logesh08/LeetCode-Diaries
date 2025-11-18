5d8c922a4c52f6464a4714d69775a1cbf9ef7ad2
Logesh08 <logeshkrishna08@gmail.com>
2025-11-18 16:19:34 +0530

    18-Nov-2025
    closes #763

--- problems/Count Good Nodes in Binary Tree.py
+++ problems/Count Good Nodes in Binary Tree.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -14,7 +17,6 @@ class Solution:
             if node.val >= curMax:
                 nonlocal countOfGoodNodes
                 countOfGoodNodes += 1
-            if node.val > curMax:
                 curMax = node.val
             if node.left:
                 dfs(node.left, curMax)
@@ -22,5 +24,27 @@ class Solution:
                 dfs(node.right, curMax)
 
         dfs(root, root.val)
+
+        return countOfGoodNodes
+    
+
+# Using BFS
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        countOfGoodNodes = 0
+        queue = deque()
+        queue.append([root, root.val])
+
+        while queue:
+            for _ in range(len(queue)):
+                node, curMax = queue.popleft()
+                if node.val >= curMax:
+                    countOfGoodNodes += 1
+                    curMax = node.val
+
+                if node.left:
+                    queue.append([node.left, curMax])
+                if node.right:
+                    queue.append([node.right, curMax])
 
         return countOfGoodNodes
