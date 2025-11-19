0314f2ddff9472de52839df07b8e99ca36e8ce97
Logesh08 <logeshkrishna08@gmail.com>
2025-11-19 11:48:49 +0530

    19-Nov-2025
    closes #783

---
+++ problems/Keep Multiplying Found Values by Two.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        numsSet = set(nums)
+        while original in numsSet:
+            original *= 2
+        return original
