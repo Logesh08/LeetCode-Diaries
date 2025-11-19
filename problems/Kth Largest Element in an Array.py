42707c134b1011d1c21fc16e2f27b7f7b98f3d05
Logesh08 <logeshkrishna08@gmail.com>
2025-11-19 23:31:13 +0530

    19-Nov-2025
    closes #804

--- problems/Kth Largest Element in an Array.py
+++ problems/Kth Largest Element in an Array.py
@@ -21,3 +21,29 @@ class Solution:
             if len(minHeap) > k:
                 heapq.heappop(minHeap)
         return minHeap[0]
+    
+
+# Quick select
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k               # Translate to the k index from last, for largest
+
+        def quickSelect(left: int, right: int) -> int:
+            pivot = nums[right]    # To compare with each ones
+            ptr = left             # moving pointer to compare
+
+            for i in range(left, right):
+                if nums[i] <= pivot:
+                    nums[i], nums[ptr] = nums[ptr], nums[i] # Swapping at ptr
+                    ptr += 1
+
+            nums[right], nums[ptr] = nums[ptr], nums[right]
+
+            if ptr < k:
+                return quickSelect(ptr + 1, right)
+            elif ptr > k:
+                return quickSelect(left, ptr - 1)
+            else:
+                return nums[ptr]
+        
+        return quickSelect(0, len(nums) - 1)
