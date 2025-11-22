bf449cad32374dddc11dd0f26323f97fac02e366
Logesh08 <logeshkrishna08@gmail.com>
2025-11-22 17:21:34 +0530

    22-Nov-2025
    closes #832

---
+++ problems/Subsets.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+        def dfs(ind):
+            if ind == len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[ind])
+            dfs(ind + 1)
+            subset.pop()
+            dfs(ind + 1)
+        dfs(0)
+
+        return res
