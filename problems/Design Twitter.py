728e1876a99affa1dd5010bb1f5fad2071fb802d
Logesh08 <logeshkrishna08@gmail.com>
2025-11-22 13:01:57 +0530

    22-Nov-2025
    closes #822

---
+++ problems/Design Twitter.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+import heapq
+from typing import List, Set
+
+
+
+# Wow I've dont it myself, feels good!
+# But man it took more than 45 mins :skullllllll
+# Lets speed it up next time
+
+class User:
+
+    def __init__(self):
+        self.following: Set[int] = set()
+        self.tweets: List[List[int]] = []
+
+class Twitter:
+
+    def __init__(self):
+        self.users: dict[int, User] = defaultdict(User)
+        self.tweetTimeStamp: int = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        # self.users[userId].posts.append(tweetId)
+        self.users[userId].tweets.append([tweetId, self.tweetTimeStamp])
+        self.tweetTimeStamp += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        postMaxHeap = []
+
+        if userId not in self.users[userId].following:
+            self.users[userId].following.add(userId)
+
+        for followeeId in self.users[userId].following:
+            followee = self.users[followeeId]
+            for i in range(10):
+                if i + 1 > len(followee.tweets):
+                    break
+                tweetId, timeStamp = followee.tweets[-1 - i]
+                heapq.heappush(postMaxHeap, (-timeStamp, tweetId))
+            
+
+        newsFeed: List[int] = []
+        count = 0
+        while postMaxHeap and count < 10:
+            newsFeed.append(heapq.heappop(postMaxHeap)[1])
+            count += 1
+        return newsFeed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.users[followerId].following.add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        try:
+            self.users[followerId].following.remove(followeeId)
+        except:
+            pass
+
+    # def createUserIfNotExits(self, userId: int) -> None:
+    #     if userId not in se
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
