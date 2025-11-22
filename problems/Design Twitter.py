7f9a1c628f812ce5851d585a1a74c907c501fd3d
Logesh08 <logeshkrishna08@gmail.com>
2025-11-22 15:20:09 +0530

    22-Nov-2025
    closes #824

--- problems/Design Twitter.py
+++ problems/Design Twitter.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 import heapq
-from typing import List, Set
+from typing import List, Set, Deque, Tuple
 
 
 
@@ -66,3 +66,61 @@ class Twitter:
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
+
+
+
+
+
+
+# Using deque for better memory management
+
+# Wow it's a personal acheivement it bet 100% of submissions :tears_in_joy
+class User:
+
+    def __init__(self):
+        self.following: Set[int] = set()
+        self.tweets: Deque[int] = deque(maxlen=10)
+
+class Twitter:
+
+    def __init__(self):
+        self.users: dict[int, User] = defaultdict(User)
+        self.tweetTimeStamp: int = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        # self.users[userId].posts.append(tweetId)
+        self.users[userId].tweets.append([tweetId, self.tweetTimeStamp])
+        self.tweetTimeStamp -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        minHeap: List[Tuple] = []
+
+        self.users[userId].following.add(userId)
+
+        for follwee in self.users[userId].following:
+            if not self.users[follwee].tweets:
+                continue
+            idx = len(self.users[follwee].tweets) - 1
+            tweetId, timeStamp = self.users[follwee].tweets[idx]
+            minHeap.append((timeStamp, tweetId, follwee, idx))
+
+        heapq.heapify(minHeap)
+
+        newsFeed: List[int] = []
+        while minHeap and len(newsFeed) < 10:
+            timeStamp, tweetId, follwee, idx = heapq.heappop(minHeap)
+            newsFeed.append(tweetId)
+
+            if idx > 0:
+                tweetId, timeStamp = self.users[follwee].tweets[idx - 1]
+                heapq.heappush(minHeap, (timeStamp, tweetId, follwee, idx - 1))
+
+        return newsFeed
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.users[followerId].following.add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.users[followerId].following:
+            self.users[followerId].following.remove(followeeId)
