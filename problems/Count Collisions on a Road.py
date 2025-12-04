21c02aaf4773963bdc906676a6f11aba0ef8c33e
Logesh08 <logeshkrishna08@gmail.com>
2025-12-04 07:41:39 +0530

    04-Dec-2025
    closes #920

--- problems/Count Collisions on a Road.py
+++ problems/Count Collisions on a Road.py
@@ -1,3 +1,29 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        
+        # Feels so good that, I have recognized that this problem uses
+        # stack pattern on my own. Ahh big moment!
+        stack = []
+        collitions = 0
+
+        for movement in directions:
+            while stack and stack[-1] == 'R':
+                if movement == 'L':
+                    collitions += 2
+                    movement = 'S'
+                    stack.pop()
+                elif movement == 'S':
+                    collitions += 1
+                    stack.pop()
+                else:
+                    break
+
+            while stack and stack[-1] == 'S':
+                if movement == 'L':
+                    collitions += 1
+                    movement = 'S'
+                    stack.pop()
+                else:
+                    break
+            stack.append(movement)
+
+        return collitions
